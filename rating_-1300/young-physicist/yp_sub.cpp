3aa1f4d7168c8dd831fe6ed1b7cf25fddc0bccd1
rodrigcasio <rod.casio@outlook.com>
2026-05-12 13:42:38 -0600

    `feat:` create new script for submission

--- rating_-1300/young-physicist/yp_sub.cpp
+++ rating_-1300/young-physicist/yp_sub.cpp
@@ -1,28 +1,44 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int main () {
   
   int n;
   int n_2;
-  
   int total = 0;
-  vector<int> numbers;
 
   cin >> n;
 
   for (int i = 0; i < n; i++) {
-    while (cin >> n_2) {
-      numbers.push_back(n_2);
+    for (int j = 0; j < n; j++) {
+      cin >> n_2;
       total += n_2;
     }
   }
   
   if (total == 0) {
     cout << "YES" << endl;
-  } else {
+  } else if (total > 0 || total < 0) {
     cout << "NO" << endl;
   }
 
   return 0;
 }
+
+/*
+ *  3 
+ *  4 + 1 + 7 = 12
+ *  -2 + 4 + -1 = 1
+ *  1 + -5 + -3 = -7
+ *
+ *  = 6 = NO
+ *
+ * 3
+ * 3 + (-1) + 7 = 9
+ * (-5) + 2 + (-4) = -7
+ * 2 + (-1) + (-3) = - -2
+ * 
+ * = 0 = YES
+ *
+ * */
