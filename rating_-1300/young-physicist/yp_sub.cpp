0c2f3501b8a7765aa10fb7f4d014dfbef631febd
rodrigcasio <rod.casio@outlook.com>
2026-05-12 13:13:31 -0600

    `fix:`modify submission

--- rating_-1300/young-physicist/yp_sub.cpp
+++ rating_-1300/young-physicist/yp_sub.cpp
@@ -1,17 +1,21 @@
 #include <bits/stdc++.h>
-// using namespace std;
+using namespace std;
 
 int main () {
   
   int n;
+  int n_2;
+  
   int total = 0;
+  vector<int> numbers;
+
   cin >> n;
 
   for (int i = 0; i < n; i++) {
-    int res;
-    cin >> res;
-    
-    total += res;
+    while (cin >> n_2) {
+      numbers.push_back(n_2);
+      total += n_2;
+    }
   }
   
   if (total == 0) {
