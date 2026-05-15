3135cacb456b6ffd1595738cad36ea2be7ae6f57
rodrigcasio <rod.casio@outlook.com>
2026-05-14 23:38:38 -0600

    `progress added`: for submission

--- rating_-1300/a-beautiful-matrix/bm_sub.cpp
+++ rating_-1300/a-beautiful-matrix/bm_sub.cpp
@@ -5,19 +5,16 @@ int main () {
 
   int arr[5][5];
   int swap = 0;
-  int const size = 4;
+  int const size = 5;
 
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
       cin >> arr[i][j];
-      int d = arr[i][j];
-      
-      int valueI = i;
-      int valueJ = j;
+      cout << i << j << endl;
 
-
-      if (d == 1) {
-        
+      if (arr[i][j] == 1) {
+        cout << "Position " << i << j << "= " << arr[i][j] << endl;
+        break;
       }
     }
   }
@@ -27,16 +24,28 @@ int main () {
 
 /*
 
+        while (arr[i][j] != arr[2][2]) {
+          switch (i) {
+            case 0:{ i += 2; swap += 1; break; }
+            case 1:{ i += 1; swap += 1; break; }
+            case 2:{ i = 2; swap += 1; break; }
+            case 3:{ i -= 1; swap += 1; break; }
+            case 4:{ i -= 2; swap += 1; break; }
+          }
+          //-----------
+          switch (j) {
+            case 0:{ j += 2; swap += 1; break; }
+            case 1:{ j += 1; swap += 1; break; }
+            case 2:{ j = 2; swap += 1; break; }
+            case 3:{ j -= 1; swap += 1; break; }
+            case 4:{ j -= 2; swap += 1; break; }
+          }
   
 
   read the position where the digit is
   evaluate the position
   move the row so it can be in the middle row [1]
   
-  
-Logic
-
-
 int arr[5][5]; 
 
  -0 0 0 0 0
@@ -48,6 +57,27 @@ arr[1][4]
 
 i = 1
 j = 4
+  
+Logic
+
+ switch (i) {
+  case 0:{
+    i+= 2; swap += 1; break;
+  }
+  case 1:{
+    i+= 1; swap += 1; break;
+  }
+  case 2: {
+    i = 2; swap += 1; break;
+  }
+  case 3: {
+    i -= 1; swap += 1; break;
+  }
+  case 4: {
+    i -= 2; swap += 1; break;
+  }
+ }
+
 
  switch (i) {
   case 0:{
