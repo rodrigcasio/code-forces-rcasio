65e1fd1a1548b501673a3632d2108207948f1640
rodrigcasio <rod.casio@outlook.com>
2026-05-20 19:11:26 -0600

    `feat:` create .c for young physicist problem submission

---
+++ rating_-1300/young-physicist/yp_c_sub.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main () {
+
+  int n;
+  int n_2;
+  int total = 0;
+  const int xyz = 3;
+  
+  int sums[] = {0, 0, 0};
+
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < xyz; j++) {
+      scanf("%d", &n_2);
+      sums[j] += n_2;
+    }
+  }
+
+  if (sums[0] == 0 && sums[1] == 0 && sums[2] == 0) {
+    printf("YES\n");
+  } else {
+    printf("NO\n");
+  }
+
+  return 0;
+}
+
+/*
+ * logic:
+ *
+ * 3 (number of list of x-y-z);
+ * x | y | z
+ *
+ *  4  | 1 | 7
+ * -2  | 4 | -1   +
+ *  1  |-5 | -3
+ * --------------
+ * 3  | 0 |  3   = NO
+ *
+ * 3
+ *  3  | -1 | 7
+ * -5  | 2 | -4   +
+ *  2  |-1 | -3
+ * --------------
+ *  0 | 0 |  0   = YES
+ * 
+ * */
