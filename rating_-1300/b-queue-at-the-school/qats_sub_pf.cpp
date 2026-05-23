f1e019e638d7ff42f32156bf6c0dd8dc83f476c3
rodrigcasio <rod.casio@outlook.com>
2026-05-22 21:06:46 -0600

    `feat:` implement better aproach for swaping positions simultaneously

--- rating_-1300/b-queue-at-the-school/qats_sub_pf.cpp
+++ rating_-1300/b-queue-at-the-school/qats_sub_pf.cpp
@@ -3,34 +3,71 @@ using namespace std;
 
 int main () {
 
-  int i, n, t;
-  char temp, boy, girl;
+  int n, t;
+  char child;
+  char temp;
 
   cin >> n >> t;
   char arr[n];
-  for (i = 0; i < n; i++) {
-   cin >> arr[i];
-  }
+  // vector<char> kids;
   
-  i = 0;
+  // for (int i = 0; i < n; i++) {
+  //   cin >> child;
+  //   kids.push_back(child);
+  // }
 
-  while (i < n) {
-    if (arr[i] == 'B' && arr[i + 1] == 'G') {
-      temp = arr[i];
-      cout << "value temp: " << temp << endl;
-      arr[i] = arr[i + 1];
-      cout << "new val of arr[i]: " << arr[i] << endl;
-      arr[i + 1] = temp;
-      cout << "new val of arr[i + 1]: " << arr[i + 1] << endl;
-    }
-    if (temp )
-    i++;
-  }
+  // for (int j = 0; j < n; j++) {
+  //   kids[j];
+  //
+  //   while (kids[j] == 'B') {
+  //     if (kids[j + 1] == 'A') {
+  //       temp = kids[j];
+  //       kids[j] == kids[j + 1];
+  //       kids[j + 1] == temp;
+  //       j++;
+  //     }
+  //   }
+  // }
 
   for (int j = 0; j < n; j++) {
-    cout << arr[j];
+   cin >> arr[j];
+   cout << arr[j];
+   while (arr[j] == 'B') {
+     if (arr[j + 1] == 'A') {
+       temp = arr[j];
+       arr[j] = arr[j + 1];
+       arr[j + 1] = temp;
+       cout << "new arr[i] value: " << arr[j] << endl;
+       cout << "new arr[i + 1] value: " << arr[j + 1] << endl;
+       
+       j++;
+     }
+   }
   }
   
+  for (int j = 0; j < n; j++) {
+    
+  }
+  
+
+  // while ( < n) {
+  //   if (arr[i] == 'B' && arr[i + 1] == 'G') {
+  //     temp = arr[i];
+  //     cout << "value temp: " << temp << endl;
+  //     arr[i] = arr[i + 1];
+  //     cout << "new val of arr[i]: " << arr[i] << endl;
+  //     arr[i + 1] = temp;
+  //     cout << "new val of arr[i + 1]: " << arr[i + 1] << endl;
+  //   }
+  //   if (temp )
+  //   i++;
+  // }
+
+  for (int z = 0; z < n; z++) {
+    cout << arr[z];
+  }
+  
+  cout << endl;
   
   return 0;
 }
@@ -38,6 +75,7 @@ int main () {
 /*
 
   Logic
+
   
   1 to n queue
   n = number of children 
@@ -46,9 +84,7 @@ int main () {
   B, G, G, B, G
   
   G, B, G, G, B
-
-
   
+  from second 0 to 1 need to be changed simultaneously
   
-
 */
