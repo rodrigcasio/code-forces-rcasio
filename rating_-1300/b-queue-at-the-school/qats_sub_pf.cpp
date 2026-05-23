519615194b3ddd814953b803208a65f727970b87
rodrigcasio <rod.casio@outlook.com>
2026-05-22 22:51:25 -0600

    `feat:` implementing code logic within the input `for` loop

--- rating_-1300/b-queue-at-the-school/qats_sub_pf.cpp
+++ rating_-1300/b-queue-at-the-school/qats_sub_pf.cpp
@@ -6,35 +6,50 @@ int main () {
   int n, t;
   char child;
   char temp;
+  bool x = true;
 
   cin >> n >> t;
   char arr[n];
   for (int i = 0; i < n; i++) {
     cin >> arr[i];
-    cout << arr[i];
+    if (!x) { 
+      if (arr[i] == 'G' && arr[i - 1] == 'B') {
+        temp = arr[i];
+        arr[i] = arr[i - 1];
+        arr[i - 1] = temp;
+        bool = true;
+      }
+    }
   }
+
+
   
-  for (int j = 0; j < n; j++) {
-   cout << arr[j];
-   while (arr[j] == 'B') {
-     if (arr[j + 1] == 'A') {
-       temp = arr[j];
-       arr[j] = arr[j + 1];
-       arr[j + 1] = temp;
-       cout << "new arr[i] value: " << arr[j] << endl;
-       cout << "new arr[i + 1] value: " << arr[j + 1] << endl;
-       
-       j++;
-     }
-   }
-  }
+  
+      // while (arr[i] == 'G') {
+      //   if (arr[i - 1] == 'B') {
+      //     temp = arr[i];
+      //     arr[i] = arr[i - 1];
+      //     arr[i - 1] = temp;
+      //     cout << "new arr[i] = " << arr[i] << endl;
+      //     cout << "new arr[i - 1] = " << arr[i - 1] << endl;
+      //   }
+      // }
+  // for (int j = 0; j < n; j++) {
+  //  while (arr[j] == 'B') {
+  //    if (arr[j + 1] == 'G') {
+  //      temp = arr[j];
+  //      arr[j] = arr[j + 1];
+  //      arr[j + 1] = temp;
+  //      cout << "new arr[i] value: " << arr[j] << endl;
+  //      cout << "new arr[i + 1] value: " << arr[j + 1] << endl;
+  //    }
+  //  }
+  // }
   
   for (int w = 0; w < n; w++) {
     cout << arr[w];
   }
-  
 
-  
   cout << endl;
   
   return 0;
@@ -53,7 +68,7 @@ int main () {
     kids[j];
 
     while (kids[j] == 'B') {
-      if (kids[j + 1] == 'A') {
+      if (kids[j + 1] == 'G') {
         temp = kids[j];
         kids[j] == kids[j + 1];
         kids[j + 1] == temp;
