97e9ba0e44c4b4571b26852763977fad854803e6
rodrigcasio <rod.casio@outlook.com>
2026-05-23 12:26:07 -0600

    `feat:` write improved version for problem `queue at the school` submission

---
+++ rating_-1300/b-queue-at-the-school/qats_sub_pro.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+  
+  int n, t;
+  cin >> n >> t;
+
+  string s;
+  cin >> s;
+
+  for (int time = 0; time < t; time++) {
+    for (int j = 0; j < n - 1; j++) {     // important: time < n - 1
+      if (s[j] == 'B' && s[j + 1] == 'G') {
+        swap(s[j], s[j + 1]);
+        j++;
+      }
+    }
+  }
+
+  cout << s << endl;
+
+  return 0;
+}
