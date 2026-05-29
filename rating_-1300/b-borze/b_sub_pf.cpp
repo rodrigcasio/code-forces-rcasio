4e1508134da8d91265241e6a031f8dfc4f9fd3ee
rodrigcasio <rod.casio@outlook.com>
2026-05-29 16:09:44 -0400

    `feat:` completed first aproach and ready to test

--- rating_-1300/b-borze/b_sub_pf.cpp
+++ rating_-1300/b-borze/b_sub_pf.cpp
@@ -1,9 +1,24 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 int main () {
 
+  string s;
+  cin >> s;
   
-
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] == '.') {
+      if (s[i + 1] == '-') {
+        cout << 0;
+      }
+    } else if (s[i] == '-') {
+      if (s[i + 1] == '.') {
+       cout << 1; 
+      } else {
+        cout << 2;
+      }
+    } 
+  }
 
 
   return 0;
@@ -19,5 +34,10 @@ Logic:
 -. == 1
 
 --  == 0
+
+
+. -. --
+
+0 1 2
 
 */
