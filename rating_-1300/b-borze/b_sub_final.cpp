7f5a97809b25691577c7c6f41c23b24128a5c3d0
rodrigcasio <rod.casio@outlook.com>
2026-05-30 18:18:54 -0600

    `feat:` Finish verison for first submission `initial tests passed`

---
+++ rating_-1300/b-borze/b_sub_final.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> // 32B
+using namespace std;
+
+int main () {
+
+  string s;
+  cin >> s;
+
+  for (int i = 0; i < s.size(); i++) {
+    while (s[i] == '.') {
+      if (s[i + 1] == '-' || s[i + 1] == '.' || s[i + 1] == '\0') {
+        cout << 0;
+      } else if (s[i - 1] == '-') {
+        cout << 1;
+      }
+      i++;
+      break;
+    }
+
+    while (s[i] == '-') {
+      if (s[i + 1] == '.') {
+        cout << 1;
+      } else if (s[i + 1] == '-' || s[i - 1] == '-') {
+        cout << 2;
+      }
+      i++;
+      break;
+    }
+  }
+
+  cout << endl;
+
+  return 0;
+}
