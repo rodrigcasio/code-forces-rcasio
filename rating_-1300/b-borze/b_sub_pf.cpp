92b29ab45f4783291b16762119804b0a367a5de9
rodrigcasio <rod.casio@outlook.com>
2026-05-30 18:07:36 -0600

    `feat:` Finish succesful aproach for the initial tests

--- rating_-1300/b-borze/b_sub_pf.cpp
+++ rating_-1300/b-borze/b_sub_pf.cpp
@@ -1,12 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// .-.--
+
 int main () {
 
   string s;
   cin >> s;
   
   for (int i = 0; i < s.size(); i++) {
+    while (s[i] == '.') {
+      if (s[i + 1] == '-' || s[i + 1] == '\0') {
+        cout << "value i = " << i << "| value of next i + 1: " << s[i+1] << endl;
+        cout << 0 << endl;
+      } else if (s[i + 1] == '.' || s[i + 1] == '\0') {
+        cout << "value i = " << i << "| value of next i + 1: " << s[i+1] << endl;
+        cout << 0 << endl;
+      } else if (s[i - 1] == '-') {
+        cout << "value i = " << i << "| previous value i - 1: " << s[i-1] << endl;
+        cout << 1 << endl;
+      }
+      i++;
+      break;
+    }
+
+    while (s[i] == '-') {
+      if (s[i + 1] == '.') {
+        cout << "value i = " << i << "| value of next i + 1: " << s[i+1] << endl;
+        cout << 1 << endl;
+      } else if (s[i + 1] == '-' || s[i - 1] == '-') {
+        cout << 2 << endl;
+        cout << "value i = " << i << "| value of next i + 1: " << s[i+1] << endl;
+      }
+      i++;
+      break;
+    }
+  }
+
+  cout << endl;
+
+
+  return 0;
+}
+
+
+/*
+
+    while (s[i] == '-') {
+      if (s[i + 1] == '.') {
+        cout << 1;
+      } else if (s[i + 1] == '-') {
+        cout << 2;
+      } else {
+        cout << 0;
+      }
+    }
+
+    while (s[i] == '.') {
+      if (s[i + 1] == '.' || s[i - 1] == '-') {
+        cout << 0;
+        break;
+      }
+    }
     if (s[i] == '.' && s[i + 1] == '-') {
       cout << 0 << endl;
       i++;
@@ -20,14 +75,7 @@ int main () {
       i++;
       cout << "new value of i: " << i << endl;
     }
-  }
 
-
-  return 0;
-}
-
-
-/*
 
 Logic:
 
