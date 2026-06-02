a1fbf3681087633477e31cd6912eb6066bfab5b3
rodrigcasio <rod.casio@outlook.com>
2026-06-02 15:03:16 -0600

    `feat:` figure out the logic within this problem

--- rating_-1300/a-beautiful-year/by_sub_pf.cpp
+++ rating_-1300/a-beautiful-year/by_sub_pf.cpp
@@ -1,13 +1,28 @@
-#include <iostream>
+#include <iostream>   // 271A
+using namespace std;
 
 int main () {
 
+
+  
+  
 
   return 0;
 }
 
 /*
 
+  int y;
+  int x = 0;
+
+  cin >> y;
+
+  while (y != 0) {
+    x = y % 10;
+    y /= 10;
+    cout << "value of x: " << x << endl;
+    cout << "value of y: " << y << endl;
+  }
 
 
 */
