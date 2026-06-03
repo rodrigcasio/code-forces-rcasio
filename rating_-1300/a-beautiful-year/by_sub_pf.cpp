bcaf5b4acffecb3cbd8ed41154c58d8c8efb5d89
rodrigcasio <rod.casio@outlook.com>
2026-06-02 20:10:14 -0600

    `feat:` add first aproach for solving this problem, `not solved yet`

--- rating_-1300/a-beautiful-year/by_sub_pf.cpp
+++ rating_-1300/a-beautiful-year/by_sub_pf.cpp
@@ -3,9 +3,27 @@ using namespace std;
 
 int main () {
 
+  int y;
+  int x = 0;
+  int z = 1;
+  int u = 0;
 
-  
-  
+  cin >> y;
+
+  while (y != 0) {
+    x += 1;
+    x += (y % 10) * (z);
+    
+    u += x;
+
+    z *= 10;
+    y /= 10;
+    cout << "Value of x: " << x << endl;
+    cout << "Value of y: " << y << endl;
+    x = 0;
+  }
+
+  cout << "Total value: " << u << endl;
 
   return 0;
 }
