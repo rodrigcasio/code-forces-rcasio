bc6b318f2d3a2dcfb8f979216f5bc9547cd15785
rodrigcasio <rod.casio@outlook.com>
2026-07-02 20:27:03 -0600

    `feat:` setup first aproach

--- rating_-1300/a-lights-out/lo_sub_pf.cpp
+++ rating_-1300/a-lights-out/lo_sub_pf.cpp
@@ -6,5 +6,15 @@ int main () {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
+  int x;
+  
+  for (int i = 0; i <= 2; ++i) {
+    for (int j = 0; j <= 2; ++j) {
+      cin >> x;
+      cout << x;
+    }
+    cout << "\n";
+  }
+
   return 0;
 }
