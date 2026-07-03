fabdc38af7f251295ea2288a609348d455780a44
rodrigcasio <rod.casio@outlook.com>
2026-07-03 16:01:31 -0600

    `feat:` added new aproach

--- rating_-1300/a-lights-out/lo_sub_pf.cpp
+++ rating_-1300/a-lights-out/lo_sub_pf.cpp
@@ -8,18 +8,63 @@ int main () {
   cin.tie(0);
 
   int x;
-  
+
   for (int i = 0; i <= 2; ++i) {
     for (int j = 0; j <= 2; ++j) {
       cin >> x;
-      
-      if (x % 2 == 0) {
-        if (x == 1) {
+     
+      if (i == 0 && j == 1) {
+        if (x % 2 == 0) {
           arr[i][j] = 0;
+          arr[i+1][j] = 0;
+          arr[i][j+1] = 0;
+          arr[i][j-1] = 0;
         } else {
           arr[i][j] = 1;
+          arr[i+1][j] = 1;
+          arr[i][j+1] = 1;
+          arr[i][j-1] = 1;
         }
+      } else if (i == 1 && j == 0) {
+        if (x % 2 == 0) {
+          
+        } else {
+          
+        }
+        
+      } else if (i == 1 && j == 1) {
+        if (x % 2 == 0) {
+          
+        } else {
+          
+        }
+        
+      } else if (i == 1 && j == 2) {
+        if (x % 2 == 0) {
+          
+        } else {
+          
+        }
+        
+      } else if (i == 2 && j == 1) {
+        if (x % 2 == 0) {
+          
+        } else {
+          
+        }
+        
       }
+
+
+
+      // if (x % 2 == 0) {
+      //   if (x == 1) {
+      //     arr[i][j] = 0;
+      //
+      //   } else {
+      //     arr[i][j] = 1;
+      //   }
+      // }
       cout << arr[i][j];
     }
     cout << "\n";
