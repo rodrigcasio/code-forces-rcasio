c79d35cddaf3e583e493cfcf4d078a051ab94359
rodrigcasio <rod.casio@outlook.com>
2026-07-04 18:55:57 -0600

    `fix:` Change `out of bounds` error within evaluation i == 2 && i == 0
    wrongly pasted the correct ternary operation for adjecent lights change for position [2][0], when pressed

--- rating_-1300/a-lights-out/lo_sub_pf.cpp
+++ rating_-1300/a-lights-out/lo_sub_pf.cpp
@@ -3,177 +3,102 @@ using namespace std;
 
 int main () {
   
-  // std::vector<std::vector <int>> arr (3, std::vector<int> (3, 1));
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  // std::vector<std::vector <int>> arr (3, std::vector<int> (3, 1)); // modern way
   int arr[3][3] = {
     {1, 1, 1 },
     {1, 1, 1 },
     {1, 1, 1 }
   };
   
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
   int x;
 
   for (int i = 0; i <= 2; ++i) {
     for (int j = 0; j <= 2; ++j) {
       cin >> x;
 
-      // n = arr[i][j];
       if (x % 2 != 0) {
         if (i == 0 && j == 0) {
-          if (x % 2 != 0) {
-            if (arr[i][j] == 0) {
-              arr[i][j] = 1;
-              ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
-              ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
-            } else {
-              arr[i][j] = 0;
-              ((arr[i][j+1] == 0) ? arr[i][j+1] = 1 : arr[i][j+1] = 0);
-              ((arr[i+1][j] == 0) ? arr[i+1][j] = 1 : arr[i+1][j] = 0);
-            }
-          }
+          ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+          // innevitable changes for adjacent lights 
+          ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
+          ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
 
         } else if (i == 0 && j == 1) {
-          if (x % 2 != 0) {
-            if (arr[i][j] == 0) {
-              arr[i][j] = 1;
-              ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
-              ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
-              ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
+            // light pressed:
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+            
+            ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
+            ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
+            ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
 
-            } else {
-              arr[i][j] = 0;
-              ((arr[i+1][j] == 0) ? arr[i+1][j] = 1 : arr[i+1][j] = 0);
-              ((arr[i][j+1] == 0) ? arr[i][j+1] = 1 : arr[i][j+1] = 0);
-              ((arr[i][j-1] == 0) ? arr[i][j-1] = 1 : arr[i][j-1] = 0);
-            }
-          }
-        }
-
-        else if (i == 0 && j == 2) {
-          if (x % 2 != 0) {
-            if (arr[i][j] == 0) {
-              arr[i][j] = 1;
-              ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
-              ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
-            } else {
-                arr[i][j] = 0;
-              ((arr[i][j-1] == 0) ? arr[i][j-1] = 1 : arr[i][j-1] = 0);
-              ((arr[i+1][j] == 0) ? arr[i+1][j] = 1 : arr[i+1][j] = 0);
-            }
-          }
+        } else if (i == 0 && j == 2) {
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+ 
+            ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
+            ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
 
         } else if (i == 1 && j == 0) {
-            if (x % 2 != 0) {
-              if (arr[i][j] == 0) {
-                arr[i][j] = 1;
-                ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
-                ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
-                ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1); 
-            
-              } else {
-                arr[i][j] = 0;
-                ((arr[i+1][j] == 0) ? arr[i+1][j] = 1 : arr[i+1][j] = 0);
-                ((arr[i-1][j] == 0) ? arr[i-1][j] = 1 : arr[i-1][j] = 0);
-                ((arr[i][j+1] == 0) ? arr[i][j+1] = 1 : arr[i][j+1] = 0); 
-          
-              }
-            } 
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+            ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
+            ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
+            ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1); 
 
         } else if (i == 1 && j == 1) {
-            if (x % 2 != 0) {
-              if (arr[i][j] == 0) {
-                arr[i][j] = 1;
-                ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
-                ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
-                ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
-                ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
-              } else {
-                arr[i][j] = 0;
-                ((arr[i-1][j] == 0) ? arr[i-1][j] = 1 : arr[i-1][j] = 0);
-                ((arr[i+1][j] == 0) ? arr[i+1][j] = 1 : arr[i+1][j] = 0);
-                ((arr[i][j-1] == 0) ? arr[i][j-1] = 1 : arr[i][j-1] = 0);
-                ((arr[i][j+1] == 0) ? arr[i][j+1] = 1 : arr[i][j+1] = 0);
-              }
-            }
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+            ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
+            ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
+            ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
+            ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
 
         } else if (i == 1 && j == 2) {
-            if (x % 2 != 0) {
-              if (arr[i][j] == 0) {
-                arr[i][j] = 1;
-                ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
-                ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
-                ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
-              } else {
-                arr[i][j] = 0;
-                ((arr[i-1][j] == 0) ? arr[i-1][j] = 1 : arr[i-1][j] = 0);
-                ((arr[i][j-1] == 0) ? arr[i][j-1] = 1 : arr[i][j-1] = 0);
-                ((arr[i+1][j] == 0) ? arr[i+1][j] = 1 : arr[i+1][j] = 0);
-              }
-            }
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+            ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
+            ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
+            ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
 
         } else if (i == 2 && j == 0) {
-            if (x % 2 != 0) {
-              if (arr[i][j] == 0) {
-                arr[i][j] = 1;
-                ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
-                ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
-            
-              } else {
-                  arr[i][j] = 0;
-                ((arr[i-1][j] == 0) ? arr[i-1][j] = 1 : arr[i-1][j] = 0);
-                ((arr[i][j+1] == 0) ? arr[i][j+1] = 1 : arr[i][j+1] = 0);
-              }
-            }
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+            ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
+            ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
 
         } else if (i == 2 && j == 1) {
-            if (x % 2 != 0) {
-              if (arr[i][j] == 0) {
-                arr[i][j] = 1;
-                ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
-                ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
-                ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
-              } else  {
-                arr[i][j] = 0;
-                ((arr[i-1][j] == 0) ? arr[i-1][j] = 1 : arr[i-1][j] = 0);
-                ((arr[i][j-1] == 0) ? arr[i][j-1] = 1 : arr[i][j-1] = 0);
-                ((arr[i][j+1] == 0) ? arr[i][j+1] = 1 : arr[i][j+1] = 0);
-              }
-            }
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+            ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
+            ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
+            ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
 
         } else if (i == 2 && j == 2) {
-            if (x % 2 != 0) {
-              if (arr[i][j] == 0) {
-                arr[i][j] = 1;
-                ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
-                ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
-              
-              } else {
-                  arr[i][j] = 0;
-                  ((arr[i][j-1] == 0) ? arr[i][j-1] = 1 : arr[i][j-1] = 0);
-                  ((arr[i-1][j] == 0) ? arr[i-1][j] = 1 : arr[i-1][j] = 0);
-              }
-            }
-        }
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
 
-      }
+            ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
+            ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
+        }
+      } 
     }
   }
   
-  // for (const auto& row : arr) {
-  //   for (int n : row) {
-  //     cout << n;
+  for (const auto& row : arr) {
+    for (int n : row) {
+      cout << n;
+    }
+    cout << "\n";
+  }
+  
+  // for (int r = 0; r <= 2; ++r) {
+  //   for (int c = 0; c <= 2; ++c) {
+  //     cout << arr[r][c];
   //   }
-  //   cout << "\n";
+  //    cout << "\n";
   // }
-  
-  for (int r = 0; r <= 2; ++r) {
-    for (int c = 0; c <= 2; ++c) {
-      cout << arr[r][c];
-    }
-     cout << "\n";
-  }
 
   return 0;
 }
@@ -181,6 +106,73 @@ int main () {
 
 
 /*
+  -- 6th approach (successful shorter code)
+      if (x % 2 != 0) {
+        if (i == 0 && j == 0) {
+          ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+          // innevitable changes for adjacent lights 
+          ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
+          ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
+
+        } else if (i == 0 && j == 1) {
+            // light pressed:
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+            
+            ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
+            ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
+            ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
+
+        } else if (i == 0 && j == 2) {
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+ 
+            ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
+            ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
+
+        } else if (i == 1 && j == 0) {
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+            ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
+            ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
+            ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1); 
+
+        } else if (i == 1 && j == 1) {
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+            ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
+            ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
+            ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
+            ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
+
+        } else if (i == 1 && j == 2) {
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+            ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
+            ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
+            ((arr[i+1][j] == 1) ? arr[i+1][j] = 0 : arr[i+1][j] = 1);
+
+        } else if (i == 2 && j == 0) {
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+            ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
+            ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
+
+        } else if (i == 2 && j == 1) {
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+            ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
+            ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
+            ((arr[i][j+1] == 1) ? arr[i][j+1] = 0 : arr[i][j+1] = 1);
+
+        } else if (i == 2 && j == 2) {
+            ((arr[i][j] == 1) ? arr[i][j] = 0 : arr[i][j] = 1);
+
+            ((arr[i][j-1] == 1) ? arr[i][j-1] = 0 : arr[i][j-1] = 1);
+            ((arr[i-1][j] == 1) ? arr[i-1][j] = 0 : arr[i-1][j] = 1);
+        }
+      } 
+
+
   -- 5th approach (successful)
       cin >> x;
 
